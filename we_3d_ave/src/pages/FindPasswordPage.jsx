5e0147cb968cf774eae3576f7a4173909fe289d2
[2wls] <[wkddlwls33@naver.com]>
2024-12-05 20:59:51 +0900

    front : 비번찾기, 회원가입 페이지

---
+++ we_3d_ave/src/pages/FindPasswordPage.jsx
@@ -0,0 +1,18 @@
+// FindPasswordPage.jsx: 비밀번호 찾기 페이지
+import React from "react";
+import FindPasswordContainer from "../containers/FindPasswordContainer";
+
+// 비밀번호 찾기 페이지
+const FindPasswordPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      {/* 컨테이너와 페이지 레이아웃 */}
+      <div className="w-full max-w-2xl bg-white p-8 rounded-lg shadow-md">
+        <FindPasswordContainer />
+        
+      </div>
+    </div>
+  );
+};
+
+export default FindPasswordPage;
